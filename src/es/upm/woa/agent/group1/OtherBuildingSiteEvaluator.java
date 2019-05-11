03f4a1a617a75d3fcf07262a6555e78db9a1cd5c
isegura <i.segura@alumnos.upm.es>
2019-05-11 09:52:05 +0200

    Fixed building creation conditions

--- src/es/upm/woa/agent/group1/OtherBuildingSiteEvaluator.java
+++ src/es/upm/woa/agent/group1/OtherBuildingSiteEvaluator.java
@@ -7,7 +7,6 @@ package es.upm.woa.agent.group1;
 
 import es.upm.woa.agent.group1.map.MapCell;
 import es.upm.woa.ontology.Building;
-import es.upm.woa.ontology.Empty;
 
 import jade.core.AID;
 
@@ -31,13 +30,13 @@ class OtherBuildingSiteEvaluator implements MapCellEvaluator {
     public boolean match(MapCell candidate) {
         Set<MapCell> neighbours = graphMap.getNeighbours(candidate);
         
-        return neighbours.parallelStream().allMatch((MapCell n) -> {
+        return neighbours.parallelStream().anyMatch((MapCell n) -> {
             if (n.getContent() instanceof Building) {
                 Building neighbourBuilding = (Building) n.getContent();
                 return neighbourBuilding.getOwner().equals(tribeAID);
             }
             else {
-                return n.getContent() instanceof Empty;
+                return false;
             }
         });
     }
